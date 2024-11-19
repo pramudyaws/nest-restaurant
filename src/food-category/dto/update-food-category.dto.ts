3e1aa89219e51fb8fd4aa88b4b2e90ba26c5749c
Pramudya Wibisono <pramudyaw.serious@gmail.com>
2024-11-19 08:42:18 +0700

    fix: add food category name minimum length validation

--- src/food-category/dto/update-food-category.dto.ts
+++ src/food-category/dto/update-food-category.dto.ts
@@ -2,7 +2,7 @@ import { createZodDto } from "nestjs-zod";
 import { z } from "zod";
 
 const UpdateFoodCategorySchema = z.object({
-    name: z.string().optional(),
-}).required();
+    name: z.string().trim().min(1, { message: 'Name is required' }).optional(),
+});
 
 export class UpdateFoodCategoryDto extends createZodDto(UpdateFoodCategorySchema) { }
