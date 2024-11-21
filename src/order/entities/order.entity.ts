878f772624a6b9ce07a2117350bbe89035df0508
Pramudya Wibisono <pramudyaw.serious@gmail.com>
2024-11-21 14:29:04 +0700

    feat: add attributes in order & orderItem entities, add createOrderDto using zod

--- src/order/entities/order.entity.ts
+++ src/order/entities/order.entity.ts
@@ -1,1 +1,17 @@
-export class Order { }
+import { BaseEntity } from "src/shared/entities/base.entity";
+import { User } from "src/user/entities/user.entity";
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from "typeorm";
+import { OrderItem } from "./order-item.entity";
+
+@Entity({ name: 'orders' })
+export class Order extends BaseEntity {
+    @ManyToOne(() => User, (user) => user.orders)
+    @JoinColumn({ name: 'userId' })
+    user: User;
+
+    @Column()
+    totalPrice: number;
+
+    @OneToMany(() => OrderItem, (orderItem) => orderItem.order)
+    orderItems: OrderItem[];
+}
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { User } from 'src/user/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Food } from 'src/food/entities/food.entity';
+import { Repository } from 'typeorm';
+import { Order } from './entities/order.entity';
+import { OrderItem } from './entities/order-item.entity';
 
 @Injectable()
 export class OrderService {
+    constructor(
+        @InjectRepository(Order)
+        private readonly orderRepository: Repository<Order>,
+        @InjectRepository(OrderItem)
+        private readonly orderItemRepository: Repository<OrderItem>,
+        @InjectRepository(Food)
+        private readonly foodRepository: Repository<Food>,
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) { }
     create(createOrderDto: CreateOrderDto) {
         return 'This action adds a new order';
     }
