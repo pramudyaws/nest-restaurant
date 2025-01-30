333c37d5e08d2ddafa1d6721b8b9b23178a24709
Pramudya Wibisono <pramudyaw.serious@gmail.com>
2025-01-30 20:39:56 +0700

    refactor: remove unused attribute

--- src/notification/entities/notification.entity.ts
+++ src/notification/entities/notification.entity.ts
@@ -1,12 +1,9 @@
 import { BaseEntity } from 'src/shared/entities/base.entity';
 import { User } from 'src/user/entities/user.entity';
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Entity, Column, ManyToOne } from 'typeorm';
 
 @Entity('notifications')
 export class Notification extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id: number;
-
     @ManyToOne(() => User, (user) => user.notifications, {
         onDelete: 'CASCADE',
     })
